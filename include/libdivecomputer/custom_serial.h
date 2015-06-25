6b317131f96276f494e049d5597ff86d9f830383
Claudiu Olteanu <olteanu.claudiu@ymail.com>
2015-06-26 01:00:59 +0300

    Create a generic structure to represent any type of serial communication
    Add a structure which holds references to basic operations
    on a serial communication. This can be used to pass a set
    of function pointer callbacks in order to create a custom
    implementation for serial communication.
    
    Add a generic structure to represent the needed information
    for a serial communication.
    
    Signed-off-by: Claudiu Olteanu <olteanu.claudiu@ymail.com>

---
+++ include/libdivecomputer/custom_serial.h
@@ -0,0 +1,58 @@
+/*
+ * libdivecomputer
+ *
+ * Copyright (C) 2008 Jef Driesen
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301 USA
+ */
+
+#ifndef CUSTOM_SERIAL_H
+#define CUSTOM_SERIAL_H
+
+#include <string.h>
+
+#include "context.h"
+#include "descriptor.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+typedef struct serial_t serial_t;
+
+typedef struct dc_serial_operations_t
+{
+	//TODO in the end serial_t should be replaced with  dc_serial_t
+	int (*open) (serial_t **device, dc_context_t *context);
+	int (*close) (serial_t *device);
+	int (*read) (serial_t *device, void* data, unsigned int size);
+	int (*write) (serial_t *device, const void* data, unsigned int size);
+	int (*flush) (serial_t *device, int queue);
+} dc_serial_operations_t;
+
+typedef struct dc_serial_t {
+	dc_context_t *context;			//context
+	int fd;					//file descriptor
+	long timeout;				//timeout
+	dc_transport_t type;			//the type of the transport (USB, SERIAL, IRDA, BLUETOOTH)
+	void *data;				//specific data for serial device
+	const dc_serial_operations_t *ops;	//reference to a custom set of operations
+} dc_serial_t;
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+#endif /* CUSTOM_SERIAL_H */
